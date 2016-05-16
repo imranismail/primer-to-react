8a86836fcdb8af0f93d693f10b5f474d474eea00
Faris Amali Alis <farisamalialis@ymail.com>
2016-05-16 23:26:05 +0800

    Further stab into the belly of the beat

--- presentation/index.js
+++ presentation/index.js
@@ -40,7 +40,8 @@ const images = {
   booster: require("../assets/booster-logo.png"),
   one27labs: require("../assets/one27labs.png"),
   firstReactApp: require("../assets/first-react-app.png"),
-  danTwitter: require("../assets/dan-twitter.png")
+  danTwitter: require("../assets/dan-twitter.png"),
+  vdom: require("../assets/vdom-diagram.png")
 };
 
 preloader(images);
@@ -124,6 +125,57 @@ export default class Presentation extends React.Component {
               and recently it is being used on Twitter!
             </Text></Appear>
             <Appear><image src={images.danTwitter} margin="50px auto 0px" padding="10px" bgColor="tertiary"/></Appear>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="primer" align="center">
+            <Text textSize="2.0em" margin="25px 0px 0px" bold textColor="tertiary">
+              whats with the hype?!
+            </Text>
+            <Appear><Text textSize="1.00em" margin="25px 0px 0px" textColor="tertiary">
+              but before that, thinking in React requires a mental shift, it can't be compared to common MVC frameworks.
+            </Text></Appear>
+            <Appear><Text textSize="1.00em" margin="25px 0px 50px" textColor="tertiary">
+              the closest category it could get is the V in MVC
+            </Text></Appear>
+            <Appear><Text textSize="1.00em" margin="25px 0px 100px" textColor="tertiary">
+              what does it do then?
+            </Text></Appear>
+            <List textColor="tertiary">
+              <Appear><ListItem>it deals with rendering and rerendering</ListItem></Appear>
+              <Appear><ListItem>it leverages on VirtualDOM</ListItem></Appear>
+              <Appear><ListItem>it lets you reason about your UI in terms of components</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="primer" align="center">
+            <Heading size={1} fit caps lineHeight={1} textColor="white">
+              How does it work?
+            </Heading>
+            <Layout>
+              <Appear><Fill>
+                <Text textSize="1.00em" padding="10%" margin="25px 0px 50px" bgColor="#7986CB" textColor="tertiary">
+                  Components
+                </Text>
+              </Fill></Appear>
+              <Appear><Fill>
+                <Text textSize="1.00em" padding="10%" margin="25px 0px 50px" textColor="tertiary" align="center">
+                  ➜
+                </Text>
+              </Fill></Appear>
+              <Appear><Fill>
+                <Text textSize="1.00em" padding="10%" margin="25px 0px 50px" bgColor="#43A047" textColor="tertiary">
+                  VirtualDOM
+                </Text>
+              </Fill></Appear>
+              <Appear><Fill>
+                <Text textSize="1.00em" padding="10%" margin="25px 0px 50px" textColor="tertiary" align="center">
+                  ➜
+                </Text>
+              </Fill></Appear>
+              <Appear><Fill>
+                <Text textSize="1.00em" padding="10%" margin="25px 0px 50px" bgColor="#EF5350" textColor="tertiary">
+                  DOM
+                </Text>
+              </Fill></Appear>
+            </Layout>
           </Slide>
         </Deck>
       </Spectacle>
